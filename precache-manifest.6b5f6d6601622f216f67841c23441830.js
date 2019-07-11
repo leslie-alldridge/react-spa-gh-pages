1a2e2245bb61308a51d0b547f0b845cdf41d4319
Leslie Alldridge <leslie.alldridge@xero.com>
2019-07-12 07:55:12 +1200

    Updates

---
+++ precache-manifest.6b5f6d6601622f216f67841c23441830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f114a0f82223a5e7e0bc4a46653500cf",
+    "url": "/react-spa-gh-pages/index.html"
+  },
+  {
+    "revision": "5c539d961c642f7d991f",
+    "url": "/react-spa-gh-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-spa-gh-pages/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5c539d961c642f7d991f",
+    "url": "/react-spa-gh-pages/static/js/main.203e3f29.chunk.js"
+  },
+  {
+    "revision": "6033f67dde33f95c6e6c",
+    "url": "/react-spa-gh-pages/static/js/runtime~main.ff3fdb74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-spa-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
